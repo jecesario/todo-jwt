cbaf2a9b73947735cd457c4eed212c342a5330bc
jecesario <jean.cesario-ext@ab-inbev.com>
2023-02-18 22:54:58 -0300

    create account and task repositories

---
+++ src/main/java/com/example/todo/domain/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.example.todo.domain.repositories;
+
+import com.example.todo.domain.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
